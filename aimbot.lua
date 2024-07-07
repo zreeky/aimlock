9ee8431d78abdfcac0023800db1dd7438fc93ee4
zreeky <justin.bchd@gmail.com>
2024-07-06 22:01:50 -0400

    Update aimbot.lua

--- aimbot.lua
+++ aimbot.lua
@@ -1,7 +1,7 @@
 --[[
 
-	Universal Aimbot Module by Exunys © CC0 1.0 Universal (2023 - 2024)
-	https://github.com/Exunys
+	Universal Aimlock Module by Zreeky © CC0 1.0 Universal (2023 - 2024)
+	https://github.com/zreeky
 
 ]]
 
